80a9f5a0f1203c0bb08f8d910b9b0ea0210813d1
Aurelien Labrosse <aurelien@skima.fr>
2020-01-20 14:10:04 +0100

    chore: Fix build

--- web/build.php
+++ web/build.php
@@ -3,18 +3,22 @@
 //putenv ( string $setting ) : bool
 
 $id=uniqid();
-mkdir("/tmp/build-$id");
-chdir("/tmp/build-$id");
+$sourceDir = "/tmp/build-$id";
+mkdir($sourceDir);
 
-$return = exec("cp -r /var/lib/tinysensor/tinySensor .");
-chdir("/tmp/build-$id/tinySensor/");
-shell_exec("git pull --rebase");
-shell_exec("git submodule update");
+$return = shell_exec("cp -rv /var/lib/tinysensor/tinySensor $sourceDir");
+$sourceDir .= "/tinySensor/";
 
-mkdir("/tmp/build-$id/tinySensor/build");
-chdir("/tmp/build-$id/tinySensor/build");
+chdir($sourceDir);
 
-$config_out = shell_exec("/tmp/build-$id/tinySensor/init_avr.sh --DCMAKE_BUILD_TYPE=Release");
+$update_out =  shell_exec("git pull --rebase");
+$update_out .= shell_exec("git submodule update -f");
+
+$buildDir = $sourceDir . "/build";
+mkdir($buildDir);
+chdir($buildDir);
+
+$config_out = shell_exec("$sourceDir/init_avr.sh");
 $compilation_out = shell_exec("make -j4");
 
 preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $config_out);
@@ -24,7 +28,6 @@ preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $compilation_out);
 
 mkdir("/var/www/html/data/$id", 0777, true);
 copy("/tmp/build-$id/tinySensor/build/tinySensor-attiny84a.hex", "/var/www/html/data/$id/tinySensor-attiny84a.hex");
-shell_exec("rm -rf /tmp/build-$id/");
 ?>
 <!doctype html>
 <html lang="en">
@@ -49,6 +52,11 @@ shell_exec("rm -rf /tmp/build-$id/");
 
 <div style="height: 300px">
 <div id="accordion">
+
+
+  <h3>Code update logs</h3>
+  <div><p><pre><?=$update_out ?></pre></p></div>
+
   <h3>Configuration logs</h3>
   <div><p><pre><?=$config_out ?></pre></p></div>
 
